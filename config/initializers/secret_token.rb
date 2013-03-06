3cf583cc76c7f52b4feb0810d8caaed28419aa94
ilunari <holybelleternity@gmail.com>
2013-03-06 18:05:47 +0900

    Changed FirstApp name to Spread in order to reflect the project name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'b593254266a6db59a13ab0f5bb88ad242851397d09c0fae5968c1beefaf9961c01ad1f605cc0830168c4afad0a7fbae9f2447c5c48d0a415c9cc0e33cf6d4310'
+Spread::Application.config.secret_token = 'b593254266a6db59a13ab0f5bb88ad242851397d09c0fae5968c1beefaf9961c01ad1f605cc0830168c4afad0a7fbae9f2447c5c48d0a415c9cc0e33cf6d4310'
